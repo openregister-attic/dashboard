9e8e33e08808cb80d79cce4f847457e79a80aad7
robmckinnon <rob@movingflow>
2016-05-09 12:48:46 +0100

    Add HTTP Basic authentication
    Configure via USER and PASS environment variables.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 require 'openregister'
 
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+
+  http_basic_authenticate_with name: ENV['USER'], password: ENV['PASS'] if Rails.env.production?
   protect_from_forgery with: :exception
 
   def home
