89c7db56af63e2dd6f3d0f6040d9300cdf10174a
robmckinnon <rob@movingflow>
2016-05-13 15:56:08 +0100

    Store public bodies in Rails cache for 1 day
    Store public bodies in Rails cache for performance.
    
    Configure test environment cache store to be `:memory_store`.

--- app/services/public_bodies.rb
+++ app/services/public_bodies.rb
@@ -1,10 +1,12 @@
 class PublicBodies
 
   def call
-    register = OpenRegister.register 'public-body', from_openregister: true
-    public_bodies = register._all_records
-    public_bodies.sort_by!(&:name)
-    public_bodies
+    Rails.cache.fetch('public-bodies', expires_in: 1.day) do
+      register = OpenRegister.register 'public-body', from_openregister: true
+      public_bodies = register._all_records
+      public_bodies.sort_by!(&:name)
+      public_bodies
+    end
   rescue SocketError => e
     if Rails.configuration.stub_registers_api_when_offline
       Struct.new('PublicBody', :public_body, :name) unless defined? Stuct::PublicBody
--- spec/services/public_bodies_spec.rb
+++ spec/services/public_bodies_spec.rb
@@ -3,13 +3,26 @@ require 'openregister'
 
 RSpec.describe PublicBodies do
 
-  let(:cabinet_office) { double(name: 'Cabinet Office') }
-  let(:moj) { double(name: 'Ministry of Justice') }
+  Struct.new('PublicBody', :public_body, :name) unless defined? Stuct::PublicBody
+
+  let(:cabinet_office) { Struct::PublicBody.new('cabinet-office', 'Cabinet Office') }
+  let(:moj) { Struct::PublicBody.new('ministry-of-justice', 'Ministry of Justice') }
   let(:register) { double(_all_records: [moj, cabinet_office]) }
+
+  after { Rails.cache.clear }
 
   it 'returns public bodies from API calls ordered by name' do
     expect(OpenRegister).to receive(:register).and_return register
 
     expect(PublicBodies.new.call).to eq([cabinet_office, moj])
   end
+
+  it 'returns public bodies from cache when called a second time' do
+    expect(OpenRegister).to receive(:register).and_return register
+    PublicBodies.new.call
+
+    expect(OpenRegister).not_to receive(:register)
+    expect(PublicBodies.new.call).to eq([cabinet_office, moj])
+  end
+
 end
